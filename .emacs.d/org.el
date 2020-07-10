ab9134923f458f6daf11ec1a43463f46a312ebfd
build2stone <seggers@protonmail.com>
2020-07-10 08:47:59 +0200

    Fix latex fragment previews
    (changed \\documentclass{article} to \\documentclass[dvisvgm]{article})

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -88,6 +88,25 @@
 		       :image-converter
 		       ("dvisvgm %f -n -b min -c %S -o %O")))
 
+(setq org-format-latex-header "\\documentclass[dvisvgm]{article}
+\\usepackage[usenames]{color}
+[PACKAGES]
+[DEFAULT-PACKAGES]
+\\pagestyle{empty}             % do not remove
+% The settings below are copied from fullpage.sty
+\\setlength{\\textwidth}{\\paperwidth}
+\\addtolength{\\textwidth}{-3cm}
+\\setlength{\\oddsidemargin}{1.5cm}
+\\addtolength{\\oddsidemargin}{-2.54cm}
+\\setlength{\\evensidemargin}{\\oddsidemargin}
+\\setlength{\\textheight}{\\paperheight}
+\\addtolength{\\textheight}{-\\headheight}
+\\addtolength{\\textheight}{-\\headsep}
+\\addtolength{\\textheight}{-\\footskip}
+\\addtolength{\\textheight}{-3cm}
+\\setlength{\\topmargin}{1.5cm}
+\\addtolength{\\topmargin}{-2.54cm}")
+
 ;; Move latex preview files to ~/.cache
 (setq org-preview-latex-image-directory "~/.cache/emacs/ltximg/")
 
