bbe219e2a3256f95ede08803c0b37b5fb5dbdc65
build2stone <seggers@protonmail.com>
2020-11-17 21:29:38 +0100

    Use local mathjax

--- .doom.d/orgtweaks.el
+++ .doom.d/orgtweaks.el
@@ -84,6 +84,7 @@
          :publishing-directory "~/org/roam_html/"
          :recursive t
          :publishing-function org-html-publish-to-html
+		 :html-mathjax-options ((path "mathjax2/MathJax.js?config=TeX-AMS_HTML") (scale "100") (align "center") (font "TeX") (linebreaks "false") (autonumber "AMS") (indent "0em") (multlinewidth "85%") (tagindent ".8em") (tagside "right"))
          :headline-levels 4
          :auto-preamble t
          :preparation-function (lambda (args)
