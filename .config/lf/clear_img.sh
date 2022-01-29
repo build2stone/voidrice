10ce78ac69315313c911e66433ea6221796c9cfc
build2stone <seggers@protonmail.com>
2022-01-29 15:19:57 +0100

    lf: smoothly transitioning image previews

--- .config/lf/clear_img.sh
+++ .config/lf/clear_img.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 if [ -n "$FIFO_UEBERZUG" ]; then
+
+  if [ "$1" != "$f" ]; then
+    case "$(file -Lb --mime-type -- "$f")" in
+      image/*) exit;;
+    esac
+  fi
+
   printf '{"action": "remove", "identifier": "preview"}\n' >"$FIFO_UEBERZUG"
 fi
