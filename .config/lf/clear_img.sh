2f619eb5c00e6ad55bfb2cf636de3cf631886c67
build2stone <seggers@protonmail.com>
2022-01-29 19:14:36 +0100

    More intelligent lf image preview cleanup
    Skip cleanup if file has a cached preview, is a video, cbz or kra file

--- .config/lf/clear_img.sh
+++ .config/lf/clear_img.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 if [ -n "$FIFO_UEBERZUG" ]; then
 
+  hash() {
+    printf '%s/.cache/lf/%s' "$HOME" \
+      "$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}')"
+  }
+
+  # Don't skip cleanup if previous ($1) and current ($f) file are the same
   if [ "$1" != "$f" ]; then
+    # Skip cleanup if current file has cached preview
+    if [ -f "$(hash "$f").jpg" ]; then
+      exit
+    fi
+    # Skip cleanup if current file can be previewed
     case "$(file -Lb --mime-type -- "$f")" in
-      image/*) exit;;
+      image/*|video*) exit;;
+      application/zip)
+        case "${f##*.}" in
+          cbz|kra) exit;;
+        esac
+        ;;
     esac
   fi
 
